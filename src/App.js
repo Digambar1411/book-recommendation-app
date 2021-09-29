f4493a50df5881ab2cbad71ed2869ce6ea82f43e
Digambar <53257891+Digambar1411@users.noreply.github.com>
2021-09-29 21:52:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import React from "react";
+
+var userName="Digambar";
+var color= e";
+
+export default function App() {
+
+
+  return (
+    <div className="App">
+      <h1 style={{backgroundColor : color }} > welcome {userName} </h1>
+  
+       
+    </div>
+  );
+}
